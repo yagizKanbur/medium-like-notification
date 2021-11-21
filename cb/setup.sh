d0f4f9b70005d79760158ae54708e15ccfeecaa7
Yagiz Kanbur <150202055@kocaeli.edu.tr>
2021-11-21 15:19:21 +0300

    Add files via upload

---
+++ cb/setup.sh
@@ -0,0 +1,20 @@
+echo 'Couchbase preperation started..'
+res=`docker ps | grep couchbase`
+read -ra arr <<<"$res"
+echo $arr
+# prepare couchbase
+container=$arr
+
+docker exec -t $container couchbase-cli cluster-init --cluster-name medium-cluster \
+   --cluster-username Administrator --cluster-password password --services data,index,query \
+   --cluster-ramsize 1024
+
+docker exec -t $container couchbase-cli bucket-create -c localhost -u Administrator -p password \
+  --bucket user_bucket --bucket-type couchbase --bucket-ramsize 512
+
+docker exec -t $container couchbase-cli bucket-create -c localhost -u Administrator -p password \
+  --bucket article_bucket --bucket-type couchbase --bucket-ramsize 512
+
+docker exec -t $container couchbase-cli user-manage -c localhost -u Administrator -p password \
+  --set --rbac-username Administrator --rbac-name Administrator --rbac-password password --roles admin \
+  --auth-domain local
